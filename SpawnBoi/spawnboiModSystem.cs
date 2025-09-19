c8a3093ed8c70981848d199bbe8993f1dd9b3322
CARNY666 <b.e.cuthbertson@gmail.com>
2025-09-19 18:23:05 -0400

    Get ready for others.

---
+++ SpawnBoi/spawnboiModSystem.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Vintagestory.API.Common;
+using Vintagestory.API.Config;
+using Vintagestory.API.MathTools;
+using Vintagestory.API.Server;
+using Vintagestory.API.Common.Entities;
+using Vintagestory.API.Datastructures;
+using Vintagestory.GameContent;
+
+namespace spawnboi
+{
+    public class spawnboiModSystem : ModSystem
+    {
+        ICoreServerAPI sapi;
+        public override void Start(ICoreAPI api)
+        {
+            if (api is ICoreServerAPI serverApi)
+            {
+                sapi = serverApi;
+                //sapi.RegisterCommand("startdig", "Starts a boring machine that digs a tunnel", "[blockcode] [optional: dropItems]", StartDigCommand, Privilege.chat);
+                //sapi.RegisterCommand("stopdig", "Stops your active boring machine", "", StopDigCommand, Privilege.chat);
+                //sapi.RegisterCommand("digspeed", "Set the speed of your active boring machine in blocks per second (1-250)", "<bps>", DigSpeedCommand, Privilege.chat);
+
+                // Update ?? every 200ms (~5 ticks per second). Actual movement uses dt for precision                
+                //sapi.Event.RegisterGameTickListener(UpdateDiggers, 200);
+
+
+
+            }
+        }
+
+    }
+}
